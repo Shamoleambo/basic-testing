4f0dfe821c4d4f72465dc187c0b211286b17ffad
Tiago Gomes <tidgomes@gmail.com>
2024-01-08 15:26:29 -0300

    write more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -6,3 +6,15 @@ it('should summarize all number values in an array', () => {
   const result = add(numbers)
   expect(result).toBe(6)
 })
+
+it('should yield NaN if at least one invalid number is provided', () => {
+  const addArgs = [1, 'string']
+  const result = add(addArgs)
+  expect(result).toBeNaN()
+})
+
+it('should yield the correct answer from a numeric string array', () => {
+  const addArgs = ['1', '2']
+  const result = add(addArgs)
+  expect(result).toBe(3)
+})

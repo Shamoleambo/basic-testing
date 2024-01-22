8608e095909c1379e4b733aac39878ec608237da
Tiago Gomes <tidgomes@gmail.com>
2024-01-22 13:18:30 -0300

    test undefined return value for validateNumber

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,9 +1,16 @@
-import { validateStringNotEmpty } from './validation'
+import { validateStringNotEmpty, validateNumber } from './validation'
 import { it, expect } from 'vitest'
 
 it('should return nothing if value is valid', () => {
   const input = 'valid value'
   const result = validateStringNotEmpty(input)
+
+  expect(result).toBeFalsy()
+})
+
+it('should return nothing if value is valid', () => {
+  const input = 1
+  const result = validateNumber(input)
 
   expect(result).toBeFalsy()
 })
